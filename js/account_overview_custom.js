c6c21688f43ec064881791fe6ea9b22f065aac0a
Kanav Khurana <kanavkhurana@gmail.com>
2017-06-30 12:51:10 +0530

    change to overview: templatised the bu bubbles

---
+++ js/account_overview_custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  /*Populate BU bubbles*/
+  var mshbubbletemplate = document.querySelector('#mshbubble_overview');
+  var uspbubbletemplate = document.querySelector('#uspbubble_overview');
+
+  /*Reuse the bubbles*/
+  var mshbubble=undefined;
+  mshbubble = document.importNode(mshbubbletemplate.content, true);
+  mshbubbleli = document.createElement("li");
+  mshbubbleli.appendChild(mshbubble);
+
+  document.getElementById('bulisting').appendChild(mshbubbleli);
+
+  var uspbubble=undefined;
+  uspbubble = document.importNode(uspbubbletemplate.content, true);
+  uspbubbleli = document.createElement("li");
+  uspbubbleli.appendChild(uspbubble);
+
+  document.getElementById('bulisting').appendChild(uspbubbleli);
+  /*mshbubble=undefined;
+  mshbubble = document.importNode(mshbubbletemplate.content, true);
+  document.getElementById('leadrow2subrow1').appendChild(mshbubble);
+
+  var uspbubble=undefined;
+  uspbubble = document.importNode(uspbubbletemplate.content, true);
+  document.getElementById('leadrow2subrow2').appendChild(uspbubble);*/
+
+});
